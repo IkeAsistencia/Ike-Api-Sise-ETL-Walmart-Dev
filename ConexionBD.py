d6c541a608f9ffca1cd0813e64e039f3caf0589b
IKE-RicardoSanchez <rmartin@ikeasistencia.com>
2025-11-27 17:35:41 -0600

    cambio de driver de conexion SQL18

--- ConexionBD.py
+++ ConexionBD.py
@@ -4,7 +4,8 @@ import os
 def conectar():
     conexion=(
         #'DRIVER={SQL Server};'
-        'DRIVER={FreeTDS};'
+        #'DRIVER={FreeTDS};'
+        'DRIVER={ODBC Driver 18 for SQL Server};'
         f'SERVER={os.getenv("MX_DB_SERVER")};'
         f'DATABASE={os.getenv("MX_DB_NAME")};'
         f'UID={os.getenv("MX_DB_USER")};'
