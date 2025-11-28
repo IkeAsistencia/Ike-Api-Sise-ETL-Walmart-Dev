bd6ac29cac5bab8202f14db02e99847224850e71
IKE-RicardoSanchez <rmartin@ikeasistencia.com>
2025-11-27 18:16:37 -0600

    cambio de driver de conexion freetds

--- ConexionBD.py
+++ ConexionBD.py
@@ -4,8 +4,9 @@ import os
 def conectar():
     conexion=(
         #'DRIVER={SQL Server};'
-        'DRIVER={FreeTDS};'
+        #'DRIVER={FreeTDS};'
         #'DRIVER={ODBC Driver 18 for SQL Server};'
+        'DRIVER={Microsoft ODBC Driver 18 for SQL Server};'
         f'SERVER={os.getenv("MX_DB_SERVER")};'
         f'DATABASE={os.getenv("MX_DB_NAME")};'
         f'UID={os.getenv("MX_DB_USER")};'
