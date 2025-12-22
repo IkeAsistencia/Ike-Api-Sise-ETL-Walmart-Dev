c95c488de9e9190850f5d750708a43a127f27524
IKE-RicardoSanchez <rmartin@ikeasistencia.com>
2025-12-22 13:46:11 -0600

    update driver sql 17

--- ConexionBD.py
+++ ConexionBD.py
@@ -6,15 +6,15 @@ def conectar():
     conexion=(
         #'DRIVER={SQL Server};'
         #'DRIVER={FreeTDS};'
-        'DRIVER={ODBC Driver 18 for SQL Server};'
+        'DRIVER={ODBC Driver 17 for SQL Server};'
         #'DRIVER={Microsoft ODBC Driver 18 for SQL Server};'
         f'SERVER={os.getenv("MX_DB_SERVER")},{os.getenv("MX_DB_PORT")};'
         f'DATABASE={os.getenv("MX_DB_NAME")};'
         f'UID={os.getenv("MX_DB_USER")};'
         f'PWD={os.getenv("MX_DB_PASSWORD")};'
         'CHARSET=UTF8MB4;'
-        #"Encrypt=yes;"
-        #"TrustServerCertificate=yes;"
+        "TrustServerCertificate=yes;"
+        "Encrypt=yes;"
     )
     return pyodbc.connect(conexion,timeout=300)
 
