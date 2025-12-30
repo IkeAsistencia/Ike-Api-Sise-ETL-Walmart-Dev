c035343f78f98084ca6f1b6c74668bcd3120ac79
IKE-RicardoSanchez <rmartin@ikeasistencia.com>
2025-12-30 17:44:17 -0600

    cambio de driver de conexion freeTDS

--- ConexionBD.py
+++ ConexionBD.py
@@ -5,15 +5,15 @@ def conectar():
     
     conexion=(
         #'DRIVER={SQL Server};'
-        #'DRIVER={FreeTDS};'
-        'DRIVER={ODBC Driver 17 for SQL Server};'
+        'DRIVER={FreeTDS};'
+        #'DRIVER={ODBC Driver 17 for SQL Server};'
         #'DRIVER={Microsoft ODBC Driver 18 for SQL Server};'
         f'SERVER={os.getenv("MX_DB_SERVER")},{os.getenv("MX_DB_PORT")};'
         f'DATABASE={os.getenv("MX_DB_NAME")};'
         f'UID={os.getenv("MX_DB_USER")};'
         f'PWD={os.getenv("MX_DB_PASSWORD")};'
         'CHARSET=UTF8MB4;'        
-        "Encrypt=no;"
+        #"Encrypt=no;"
         #"TrustServerCertificate=yes;"
     )
     return pyodbc.connect(conexion,timeout=300)
