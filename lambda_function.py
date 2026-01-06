ae52913e64857d3ce2c327153dc07ed23ddd9317
IKE-RicardoSanchez <rmartin@ikeasistencia.com>
2026-01-06 17:50:29 -0600

    Ajuste FreeTDS5

--- lambda_function.py
+++ lambda_function.py
@@ -46,6 +46,14 @@ def lambda_handler(event, context):
     logger.info("Drivers ODBC disponibles: %s", drivers)
     print("Drivers ODBC disponibles P:", drivers)
 
+    import socket
+    s = socket.socket()
+    s.settimeout(5)
+    s.connect((os.getenv("MX_DB_SERVER"), int(os.getenv("MX_DB_PORT"))))
+    print("Conexión TCP OK")
+
+
+
     #logger.info("Existe /opt/etc/odbcinst.ini: %s", os.path.exists("/opt/etc/odbcinst.ini"))
     #logger.info("Existe libmsodbcsql en /opt/lib: %s", any('libmsodbcsql' in f for f in os.listdir('/opt/lib')))
     #logger.info("LD_LIBRARY_PATH: %s", os.environ.get('LD_LIBRARY_PATH'))
